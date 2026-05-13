33a9342a4e308c56ca6d36e1809c5465e5451d80
jerome <jeje.bertin@orange.fr>
2026-05-13 13:42:32 +0200

    Validation et Relations : Ajout class-validator, Entity Post et lien User-Post

--- backend/src/modules/posts/dto/create-post.dto.ts
+++ backend/src/modules/posts/dto/create-post.dto.ts
@@ -1,1 +1,15 @@
-export class CreatePostDto {}
+import { IsString, MinLength } from 'class-validator';
+
+export class CreatePostDto {
+
+    @IsString()
+	@MinLength(1)
+	title: string;
+
+	@IsString()
+	@MinLength(1)
+	description: string;
+
+
+}
+
--- backend/src/modules/posts/dto/update-post.dto.ts
+++ backend/src/modules/posts/dto/update-post.dto.ts
@@ -1,4 +1,16 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreatePostDto } from './create-post.dto';
+import { IsOptional, IsString, MinLength } from 'class-validator';
 
-export class UpdatePostDto extends PartialType(CreatePostDto) {}
+export class UpdatePostDto extends PartialType(CreatePostDto) {
+
+    @IsOptional()
+	@IsString()
+	@MinLength(1)
+	title: string;
+
+	@IsOptional()
+	@IsString()
+	@MinLength(1)
+	description: string;
+}
