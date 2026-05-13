201b87e8a8c8e1f874fe01e539119a6f186843df
jerome <jeje.bertin@orange.fr>
2026-05-13 11:18:39 +0200

    user et post

--- backend/src/modules/posts/entities/post.entity.ts
+++ backend/src/modules/posts/entities/post.entity.ts
@@ -1,1 +1,18 @@
-export class Post {}
+import {
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+} from 'typeorm';
+
+    @Entity('posts')
+export class Post {
+	@PrimaryGeneratedColumn()
+	id: number;
+
+	@Column({ length: 255 })
+	title: string;
+
+	@Column({ type: 'text' })
+	descritption: string;
+
+}
--- backend/src/modules/users/entities/user.entity.ts
+++ backend/src/modules/users/entities/user.entity.ts
@@ -1,1 +1,24 @@
-export class User {}
+
+import{
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+
+}from 'typeorm';
+
+@Entity('users')
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({length: 100})
+    name: string;
+
+    @Column({unique: true, length: 150})
+    email: string;
+
+    @Column({select: false})
+    password: string;
+
+    
+}
